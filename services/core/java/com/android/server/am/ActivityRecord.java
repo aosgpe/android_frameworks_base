262348265aa71142f01bac9d4747abb3c32cd7a4
dimusa6 <dimusa.dms@gmail.com>
2019-03-24 13:36:42 +0100

    Fix Aspect radio

--- services/core/java/com/android/server/am/ActivityRecord.java
+++ services/core/java/com/android/server/am/ActivityRecord.java
@@ -2373,12 +2373,6 @@ final class ActivityRecord extends ConfigurationContainer implements AppWindowCo
             return;
         }
 
-        if(info.applicationInfo.targetSdkVersion < O) {
-            try {
-                maxAspectRatio = Float.parseFloat(SystemProperties.get("persist.sys.max_aspect_ratio.pre_o", ""));
-            } catch (Throwable t) {}
-            Log.d("PHH", "Overrode aspect ratio because pre-o to " + maxAspectRatio);
-        }
         // We must base this on the parent configuration, because we set our override
         // configuration's appBounds based on the result of this method. If we used our own
         // configuration, it would be influenced by past invocations.
